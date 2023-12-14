40c8aec9b5fad016524e967c0708e705ee401969
Jakub SQTX Sitarczyk <sssqtx@gmail.com>
2023-12-14 18:49:57 +0100

    Create function to create new Client (postgres) object

--- routes/test_db.js
+++ routes/test_db.js
@@ -1,40 +1,11 @@
-const users = [
-  {
-    userID: "1",
-    firstName: "Marcin",
-    lastName: "Grognałer",
-    profilePhoto: "friend-1.jpg",
-    email: "BiutiBlondi@wp.pl",
-    password: "RealMarditPL",
-  },
-
-  {
-    userID: "2",
-    firstName: "Marta",
-    lastName: "Skura",
-    profilePhoto: "friend-2.jpg",
-    email: "taxiNova@onet.com",
-    password: "MiroGosc123",
-  },
-
-  {
-    userID: "3",
-    firstName: "Marcinek",
-    lastName: "Ściemniacz",
-    profilePhoto: "friend-3.jpg",
-    email: "mikolajekCzar@gmail.com",
-    password: "dupa123",
-  },
-
-  {
-    userID: "4",
-    firstName: "Bob",
-    lastName: "Budowaniczy",
-    profilePhoto: "",
-    email: "mikolajekCzar@gmail.com",
-    password: "dupa123",
-  },
-];
+// const conversation = [
+//   {
+//     convID: "1",
+//     userID: user,
+//     friendID: users[0],
+//     inboxID: inbox,
+//   }
+// ]
 
 const inbox = [
   {
@@ -62,6 +33,80 @@ const inbox = [
     messageTxt: "No to spoxon",
     sentAt: "2023-12-27_07:35:52",
   },
+  {
+    authorID: "Daniel",
+    messageTxt: "Xddd",
+    sentAt: "2023-12-27_07:36:52",
+  },
 ]
 
-module.exports = {users, inbox};
+
+// const foo = "'S'";
+// const foo = "'S' OR age = 5";
+
+// const addUserQuery = {
+//   text: `INSERT INTO public."user"(firstname, lastname, "profilePhoto", email, password)
+//   VALUES ($1, $2, $3, $4, $5)`,
+// //   values: ['Marcinek', 'Ściemniacz', 'friend-3.jpg', 'mikolajekCzar@gmail.com', 'dupa123'],
+//   values: ['Marta', 'Skura', 'friend-2.jpg', 'taxiNova@onet.com', 'MiroGosc123'],
+// }
+
+
+// client.connect()
+// .then(() => console.log("Connected successfuly"))
+// .then(() => client.query(`SELECT name FROM test WHERE name = ${foo}`))
+// .then(() => client.query(`SELECT * FROM public."user"`))
+// .then(() => client.query(`SELECT * FROM public."user" WHERE "userID" = 3`))
+// .then(() => client.query(`SELECT * FROM public."inbox"`))
+// .then(() => client.query(`SELECT * FROM public."conversation"`))
+// .then(() => client.query(`SELECT * FROM public."message"`))
+// .then(results => console.table(results.rows))
+// .then(() => client.query(addUserQuery))
+// .then(results => console.table(results.rows))
+// .catch(e => console.log(e))
+// .finally(() => client.end())
+
+
+
+// **********************************************************
+// -- INSERT INTO public."user"(firstname, lastname, "profilePhoto", email, password)
+// -- VALUES 		('Grzegorz', 'Braun', 'gasnicaPolski.jpg', 'konf@wp.pl', 'haslo33');
+
+// -- GRANT ALL PRIVILEGES ON TABLE public."user" TO postgres;
+
+// -- SELECT * FROM public."user" WHERE "userID" = 1;
+
+
+
+// SELECT "table_name", "column_name", "data_type"
+// FROM information_schema.columns
+// WHERE "tables_name" = public."conversation";
+// **********************************************************
+// INSERT INTO public.inbox("lastSentAuthor", "lastMessTime", "lastMessText")
+// VALUES 		(1, '2022-10-10 11:30:30', 'Halo Halo');
+// **********************************************************
+// -- INSERT INTO public.conversation(
+// -- 	"userID", "friendID", "inboxID")
+// -- 	VALUES (1, 3, 1);
+
+// SELECT * FROM public.conversation;
+// **********************************************************
+// INSERT INTO public.message(
+// 	"messTxt", "authorID", "sentAt")
+// 	VALUES ('Szczęść boże', 3, '2022-10-10 11:30:30');
+// **********************************************************
+const {Client} = require('pg');
+
+// Function to create new Client object (needed to single query)
+function createClientDB() {
+  const client = new Client({
+    user: "postgres",
+    password: "sqtx7177",
+    host: "localhost",
+    port: 5432,
+    database: "WebChatApp_DB"
+  })
+  return client;
+}
+
+module.exports = {inbox, createClientDB};
