0339d34e04567845799a6cad124844966c42ddac
Jakub SQTX Sitarczyk <sssqtx@gmail.com>
2023-12-27 14:09:20 +0100

    Create function to get and send new added frind mail address

--- public/js/addFriend.js
+++ public/js/addFriend.js
@@ -1,3 +1,10 @@
+function sendInvit(mailInput) {
+  const txt = mailInput.value;
+  console.log("Send inv", txt);
+  //TODO
+}
+
+
 export function openAddFriendWindow() {
   const addFriendWindow = document.querySelector("section#add-friend");
   addFriendWindow.classList.add("active");
@@ -8,5 +15,27 @@ export function openAddFriendWindow() {
   addFriendWinExitBtn.addEventListener("click", () => {
     const addFriendWindow = document.querySelector("section#add-friend");
     addFriendWindow.classList.remove("active");
+  })
+
+
+  const addFriendBtn = document.getElementById("send-invit");
+  const mailInput = document.getElementById("email-input");
+
+  addFriendBtn.addEventListener('click', () => {
+    sendInvit(mailInput);  // TODO
+
+    mailInput.value = "";
+    const addFriendWindow = document.querySelector("section#add-friend");
+    addFriendWindow.classList.remove("active");
+  })
+
+  mailInput.addEventListener('keypress', (event) => {
+    if(event.key === 'Enter'){
+      sendInvit(mailInput);  // TODO
+
+      mailInput.value = "";
+      const addFriendWindow = document.querySelector("section#add-friend");
+      addFriendWindow.classList.remove("active");
+    }
   })
 }
