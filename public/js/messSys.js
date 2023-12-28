f9537ec9568361d2ecb9c1147d1eef7cb1026ff1
Jakub SQTX Sitarczyk <sssqtx@gmail.com>
2023-12-28 15:59:18 +0100

    Create function to add loading bar, load older messages and remove bar after that

--- public/js/messSys.js
+++ public/js/messSys.js
@@ -1,9 +1,15 @@
-import { getNowTime } from "./time.js";
+import { delay, getNowTime } from "./time.js";
 
 // =====================================================================================================
 // PRIVATE:
 // =====================================================================================================
-function addNewMessage(loadMode = false, amIAuthor, messTxt, isLast = false, sentAt = 0) {
+function addNewMessage(
+  loadMode = false,
+  amIAuthor,
+  messTxt,
+  isLast = false,
+  sentAt = 0
+) {
   const chat = document.getElementById("chat");
 
   const message = document.createElement("p");
@@ -16,12 +22,12 @@ function addNewMessage(loadMode = false, amIAuthor, messTxt, isLast = false, sen
   } else {
     messBubble.classList.add("friend-mess");
   }
-  if(isLast) {
+  if (isLast) {
     // Delete previous last element:
     const prevLastMess = document.getElementById("last-loaded");
-    if(prevLastMess !== null) {
-      prevLastMess.removeAttribute('id');
-      prevLastMess.removeAttribute('sentAt');
+    if (prevLastMess !== null) {
+      prevLastMess.removeAttribute("id");
+      prevLastMess.removeAttribute("sentAt");
     }
 
     // Add new last element:
@@ -31,7 +37,7 @@ function addNewMessage(loadMode = false, amIAuthor, messTxt, isLast = false, sen
   messBubble.appendChild(message);
 
   // Add messBubble to chat interface (in one of two way)
-  if(loadMode) chat.insertBefore(messBubble, chat.firstChild);
+  if (loadMode) chat.insertBefore(messBubble, chat.firstChild);
   else chat.appendChild(messBubble);
 
   // Automatic scroll down:
@@ -39,25 +45,50 @@ function addNewMessage(loadMode = false, amIAuthor, messTxt, isLast = false, sen
   messBubble.scrollIntoView({ behavior: "smooth" });
 }
 
-
 function setInboxOnChat(inboxID) {
   const chat = document.getElementById("chat");
-  chat.setAttribute("active-inbox", inboxID)
+  chat.setAttribute("active-inbox", inboxID);
 }
-
 
 function loadMessages(data) {
   console.log(data);
   // data.reverse();
   data.forEach((message, index) => {
-    const {authorID, sentAt, messTxt} = message;
+    const { authorID, sentAt, messTxt } = message;
     console.log("Wiadomość:", authorID, sentAt, messTxt);
 
-    const amIAuthor = (authorID === 1) ? true : false;
-    if(index === (data.length - 1)) addNewMessage(true, amIAuthor, messTxt, true, sentAt);  // Last loaded mess
+    const amIAuthor = authorID === 1 ? true : false;
+    if (index === data.length - 1)
+      addNewMessage(true, amIAuthor, messTxt, true, sentAt); // Last loaded mess
     else addNewMessage(true, amIAuthor, messTxt);
   });
+}
 
+
+function removeLoadBar() {
+  const loadBar = document.getElementById("load-chat");
+  if (loadBar) loadBar.remove();
+}
+
+
+function loadOlderMessages() {
+  const inboxID = chat.getAttribute("active-inbox");
+
+  const lastLoadedMessTime = document
+    .getElementById("last-loaded")
+    .getAttribute("sentat");
+
+  loadMoreMessages(inboxID, lastLoadedMessTime);
+}
+
+// Load older messages to chat:
+function loadMoreMessages(inboxID, lastMessTime = 0) {
+  if (lastMessTime === 0) lastMessTime = getNowTime();
+  const messNumber = "";
+
+  fetch(`/chat/load/mess/${inboxID}/${lastMessTime}/${messNumber}`, {
+    method: "GET",
+  }).then((r) => r.json().then((data) => loadMessages(data)));
 }
 
 // =====================================================================================================
@@ -67,11 +98,10 @@ function loadMessages(data) {
 export function clearChat() {
   const chat = document.getElementById("chat");
   const messages = chat.querySelectorAll("div.message");
-  messages.forEach(message => {
+  messages.forEach((message) => {
     message.remove();
   });
 }
-
 
 // Load new chat header:
 export function loadChatHeader(nickname, active, profilePhoto) {
@@ -96,36 +126,36 @@ export function loadChatHeader(nickname, active, profilePhoto) {
   photoImg.src = profilePhoto;
 }
 
-
 // Send new message:
 export function sendNewMessage(event, userID) {
   event.preventDefault();
-  const authorID = userID;                 // User always is author of new message. Getting userID
+  const authorID = userID; // User always is author of new message. Getting userID
   const sentAt = getNowTime();
 
   const chatTextArea = document.getElementById("write-mess");
   let messTxt = chatTextArea.value;
-  chatTextArea.value = "";                 // Clean textarea
+  chatTextArea.value = ""; // Clean textarea
 
   // If it's empty do nothing:
-  if(messTxt === "" || messTxt === "\n" || messTxt === "\r\n") return 0;
+  if (messTxt === "" || messTxt === "\n" || messTxt === "\r\n") return 0;
 
   // Add message to interface chat:
-  const itsMyMess = true;                   // TODO: To remove
+  const itsMyMess = true; // TODO: To remove
   addNewMessage(false, itsMyMess, messTxt);
 
   // Get inboxID:
   const chat = document.getElementById("chat");
-  const inboxID = chat.getAttribute('active-inbox');
+  const inboxID = chat.getAttribute("active-inbox");
 
   // Send new message with data to server:
-  console.log(`User '${authorID}' send new mess: "${messTxt}" to '${inboxID}' inbox at ${sentAt}.`);
+  console.log(
+    `User '${authorID}' send new mess: "${messTxt}" to '${inboxID}' inbox at ${sentAt}.`
+  );
   const url = `/chat/send/${inboxID}/${authorID}/${sentAt}/${messTxt}`;
   fetch(url, {
-      method: 'POST',
-  })
+    method: "POST",
+  });
 }
-
 
 // Load chat conversation with friend:
 export function loadConversation(friendID, lastMessTime = 0) {
@@ -134,16 +164,33 @@ export function loadConversation(friendID, lastMessTime = 0) {
   }).then((r) =>
     r.json().then((value) => {
       const inboxID = value.inboxID;
-      const lastMessTime = getNowTime();
+      if (lastMessTime === 0) lastMessTime = getNowTime();
       const messNumber = "";
 
       fetch(`/chat/load/mess/${inboxID}/${lastMessTime}/${messNumber}`, {
         method: "GET",
-      }).then((r) => r.json().then((data) => {
-        setInboxOnChat(inboxID);
-        loadMessages(data);
-      }));
+      }).then((r) =>
+        r.json().then((data) => {
+          setInboxOnChat(inboxID);
+          loadMessages(data);
+        })
+      );
     })
   );
 }
 
+
+export function addLoadBar() {
+  const loadBar = document.createElement("div");
+  loadBar.id = "load-chat";
+  const loadCircle = document.createElement("div");
+  loadCircle.classList.add("loader");
+
+  loadBar.appendChild(loadCircle);
+  chat.insertBefore(loadBar, chat.firstChild);
+
+  delay(850).then(() => {
+    removeLoadBar();
+    loadOlderMessages();
+  });
+}
--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,13 @@
 import { delay } from "./time.js";
 import { openTerminal, printLog } from "./terminal.js";
 import { openAddFriendWindow } from "./addFriend.js";
-import { loadChatHeader, loadConversation, clearChat, sendNewMessage } from "./messSys.js";
+import {
+  loadChatHeader,
+  loadConversation,
+  addLoadBar,
+  clearChat,
+  sendNewMessage,
+} from "./messSys.js";
 
 // =====================================================================================================
 // Constant:
@@ -18,9 +24,9 @@ settingsBtn.addEventListener("click", () => {
   menuBox.classList.toggle("active");
 
   const addUser = document.querySelector("menu li.option:nth-child(1)");
-  addUser.addEventListener('click', ()=> openAddFriendWindow());
+  addUser.addEventListener("click", () => openAddFriendWindow());
   const terminalBtn = document.querySelector("menu li.option:nth-child(2)");
-  terminalBtn.addEventListener('click', () => openTerminal());
+  terminalBtn.addEventListener("click", () => openTerminal());
 });
 
 // =====================================================================================================
@@ -28,12 +34,14 @@ settingsBtn.addEventListener("click", () => {
 const sendBtn = document.getElementById("sendBtn");
 const chatTextArea = document.getElementById("write-mess");
 // Calling the message sending function:
-sendBtn.addEventListener('click', (event) => {
+sendBtn.addEventListener("click", (event) => {
   sendNewMessage(event, userID);
-  printLog("Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque est similique rerum non esse! Unde officia totam iste laudantium tenetur.");
+  printLog(
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque est similique rerum non esse! Unde officia totam iste laudantium tenetur."
+  );
 });
-chatTextArea.addEventListener('keypress', (event) => {
-  if(event.key === 'Enter'){
+chatTextArea.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
     sendNewMessage(event, userID);
   }
 });
@@ -43,19 +51,33 @@ chatTextArea.addEventListener('keypress', (event) => {
 delay(750).then(() => {
   const friends = document.querySelectorAll("#friends-list div.friend");
   console.log(friends);
-  friends.forEach(friend => {
-    friend.addEventListener('click', () => {
+  friends.forEach((friend) => {
+    friend.addEventListener("click", () => {
       const friendID = friend.getAttribute("friendid");
       const nicknameP = friend.querySelector("span p.nickname");
       const nickname = nicknameP.innerText;
       console.log(friendID, "Przyjaciel", nickname);
 
       const imgElem = friend.querySelector("div.profile-img img");
-      const profilePhoto = imgElem.getAttribute('src');
+      const profilePhoto = imgElem.getAttribute("src");
 
-      loadChatHeader(nickname, true, profilePhoto);   // NOTE: Hard coded friend status
+      loadChatHeader(nickname, true, profilePhoto); // NOTE: Hard coded friend status
       clearChat();
       loadConversation(friendID);
-    })
+    });
   });
+});
+
+
+// =====================================================================================================
+// Load older messages after max scroll chat up:
+const chat = document.getElementById("chat");
+chat.addEventListener("scroll", (event) => {
+  let scroll = chat.scrollTop;
+
+  const loadBar = document.getElementById("load-chat");
+  console.log("Loading older messages");
+  if (scroll === 0 && !loadBar) {
+    delay(150).then(() => addLoadBar());
+  }
 });
